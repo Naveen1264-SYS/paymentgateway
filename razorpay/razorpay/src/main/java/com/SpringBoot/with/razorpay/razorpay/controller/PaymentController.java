7359e8862a90bc92a4998ba1c24d39915753e362
Naveen1264-SYS <naveen.kumar@infoservices.com>
2025-04-04 19:18:57 +0530

    Add files via upload
    created razorpay Payment gateway Backend code

---
+++ razorpay/razorpay/src/main/java/com/SpringBoot/with/razorpay/razorpay/controller/PaymentController.java
@@ -0,0 +1,28 @@
+package com.SpringBoot.with.razorpay.razorpay.controller;
+
+import com.SpringBoot.with.razorpay.razorpay.service.RazorpayService;
+import com.razorpay.RazorpayException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payments")
+public class PaymentController {
+
+    @Autowired
+    private RazorpayService razorpayService;
+
+    @PostMapping("/create-order")
+    public String createOrder(@RequestParam int amount , @RequestParam String currency){
+
+        try {
+            return razorpayService.createOrder(amount, currency, "recepient_100");
+        } catch (RazorpayException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
